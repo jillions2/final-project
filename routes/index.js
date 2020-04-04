1edc273fbe1cc4cb8a3bce1f9e4d8c887af57234
PACHARI-94 <pachari_th@cmu.ac.th>
2020-04-04 20:56:18 +0700

    3

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/',(req,res) => res.render('index'));
+router.get('/',(req,res) => res.render('welcome'));
 
 // MongoDB Atlas connection setting
 const mongoose = require('mongoose')
