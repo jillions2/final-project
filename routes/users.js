25b9a95c9657a4cf7bf7f57e216f122286aa2758
PACHARI-94 <pachari_th@cmu.ac.th>
2020-04-09 11:39:33 +0700

    11

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,9 @@ router.get('/login',(req,res) => res.render('login'));
 //Register Page
 router.get('/register',(req,res) => res.render('register'));
 
+//Home Page
+router.get('/home',(req,res) => res.render('home'));
+
 //Register Handle
 router.post('/register',(req,res) =>{ 
     console.log(req.body)
